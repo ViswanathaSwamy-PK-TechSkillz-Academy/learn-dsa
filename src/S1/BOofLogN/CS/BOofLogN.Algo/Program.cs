3c3200663bf41be7d032a8c007cf8c9a5411eefd
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-16 00:01:55 +0530

    Swamy/15may work (#7)
    * Create S2.md
    
    * Update S1.md
    
    * Update S1.md
    
    * Changes
    
    * Create S1.sln
    
    * Changes
    
    * Update S1.md
    
    * Changes
    
    * Changes

---
+++ src/S1/BOofLogN/CS/BOofLogN.Algo/Program.cs
@@ -0,0 +1,29 @@
+﻿static int BinarySearch(int[] arr, int target)
+{
+    int left = 0;
+    int right = arr.Length - 1;
+
+    while (left <= right)
+    {
+        int mid = (left + right) / 2;
+
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+        else if (arr[mid] < target)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int[] testArray = { 3, 6, 9, 12, 15, 18, 21, 24 };
+
+Console.WriteLine("Binary Search: " + BinarySearch(testArray, 9));
---
+++ src/S1/BOofOne/CS/BOofOne.Algo/Program.cs
@@ -0,0 +1,20 @@
+﻿static void ShowNameAt(string[] students, int index)
+{
+    if (index >= 0 && index < students.Length)
+    {
+        WriteLine($"Student: {students[index]} at index {index}");
+    }
+    else
+    {
+        WriteLine($"Error: Index {index} is out of bounds.");
+    }
+}
+
+string[] studentNames = [
+    "Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Hannah", "Isaac",
+    "Julia", "Kevin", "Lily", "Mike", "Nina", "Oliver", "Pamela", "Quincy", "Rachel"
+];
+
+ShowNameAt(studentNames, 17);
+
+
