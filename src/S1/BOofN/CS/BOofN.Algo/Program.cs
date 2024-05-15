dcffdc25b9af4da4bef6e7aa7b4ce999e596a16f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-16 00:20:56 +0530

    Changes

--- src/S1/BOofN/CS/BOofN.Algo/Program.cs
+++ src/S1/BOofN/CS/BOofN.Algo/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void ShowNames(string[] students)
+{
+    foreach (var student in students)
+    {
+        Console.WriteLine(student);
+    }
+}
+
+var studentNames = new string[]
+{
+    "Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Hannah", "Isaac",
+    "Julia", "Kevin", "Lily", "Mike", "Nina", "Oliver", "Pamela", "Quincy", "Rachel"
+};
+
+ShowNames(studentNames);
