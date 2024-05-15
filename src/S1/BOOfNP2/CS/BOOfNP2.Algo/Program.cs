4c39c8e729fb1bf659775a7032ba01c2d7ba4098
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-16 00:32:39 +0530

    Changes

---
+++ src/S1/BOOfNP2/CS/BOOfNP2.Algo/Program.cs
@@ -0,0 +1,39 @@
+﻿static void ShowNames(string[] students)
+{
+    foreach (var student in students)
+    {
+        WriteLine(student.ToUpper());
+    }
+
+    foreach (var student in students)
+    {
+        WriteLine(student.ToLower());
+    }
+}
+
+static void DisplayCoordinates(int size)
+{
+    string str = "";
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            str += $"({i}, {j}) ";
+        }
+        str += "\n"; // Add a newline after each row
+    }
+
+    WriteLine(str);
+}
+
+var studentNames = new string[]
+{
+    "Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Hannah", "Isaac",
+    "Julia", "Kevin", "Lily", "Mike", "Nina", "Oliver", "Pamela", "Quincy", "Rachel"
+};
+
+ShowNames(studentNames);
+
+const int size = 8;
+DisplayCoordinates(size);
