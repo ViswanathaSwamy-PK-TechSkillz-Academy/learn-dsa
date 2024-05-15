ea65084312ddee721f472051fd43bbdd1a3bc87a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-15 23:40:59 +0530

    Changes

---
+++ src/S1/BOofOne/CS/BOofOne.Algo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+string[] studentNames = {
+    "Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Hannah", "Isaac",
+    "Julia", "Kevin", "Lily", "Mike", "Nina", "Oliver", "Pamela", "Quincy", "Rachel"
+};
+
+ShowNameAt(studentNames, 17);
+
+static void ShowNameAt(string[] students, int index)
+{
+    if (index >= 0 && index < students.Length)
+    {
+        WriteLine($"Student: {students[index]} at index {index}");
+    }
+    else
+    {
+        WriteLine($"Error: Index {index} is out of bounds.");
+    }
+}
