a53485992ee244aa6c6b760d946cd315f23574fd
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-06 09:59:18 +0530

    Go lang

---
+++ src/S1/BOofOne/Go/studentHelper.go
@@ -0,0 +1,13 @@
+// studentHelper.go
+
+package main
+
+import "fmt"
+
+func showNameAt(students []string, index int) {
+	if index >= 0 && index < len(students) {
+		fmt.Printf("Student: %s at index %d\n", students[index], index)
+	} else {
+		fmt.Printf("Error: Index %d is out of bounds.\n", index)
+	}
+}
