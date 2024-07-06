0aa0c2c3d2643d6cc4dea1a58aaa85f4635d283c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-06 09:47:40 +0530

    Changes

---
+++ src/S1/BOofOne/CS/BOofOne.Algo/StudentHelper.cs
@@ -0,0 +1,18 @@
+// showNameAt.cs
+
+namespace BOofOne.Algo;
+
+public static class StudentHelper
+{
+    public static void ShowNameAt(string[] students, int index)
+    {
+        if (index >= 0 && index < students.Length)
+        {
+            WriteLine($"Student: {students[index]} at index {index}");
+        }
+        else
+        {
+            WriteLine($"Error: Index {index} is out of bounds.");
+        }
+    }
+}
