f7b058d24f4701362f2c38d52dae1bbcad33432b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-07 09:43:07 +0530

    Update studentHelper.ts

--- src/S1/BOofOne/TS/studentHelper.ts
+++ src/S1/BOofOne/TS/studentHelper.ts
@@ -4,6 +4,6 @@ export function showNameAt(students: string[], index: number): void {
     if (index >= 0 && index < students.length) {
         console.log(`Student: ${students[index]} at index ${index}`);
     } else {
-        console.log(`Error: Index ${index} is out of bounds.`);
+        console.log(`Error: Index ${index} is out of bounds of total ${students.length}.`);
     }
 }
