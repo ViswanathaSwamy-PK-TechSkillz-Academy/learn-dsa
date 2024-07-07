d553d78fcb6937b3e4579eb486ec7b4ca7957e73
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-07 09:55:25 +0530

    Update Program.cs

--- src/S1/BOofLogN/CS/BOofLogN.Algo/Program.cs
+++ src/S1/BOofLogN/CS/BOofLogN.Algo/Program.cs
@@ -2,14 +2,22 @@
 
 int[] testArray = [3, 6, 9, 12, 15, 18, 21, 24];
 
-int target = 9;
-int result = BinarySearchHelper.BinarySearch(testArray, target);
+static void PerformBinarySearch(int[] array, int target)
+{
+    int result = BinarySearchHelper.BinarySearch(array, target);
 
-if (result != -1)
-{
-    WriteLine($"Binary Search: Found target {target} at index {result}");
+    if (result != -1)
+    {
+        Console.WriteLine($"Binary Search: Found target {target} at index {result}");
+    }
+    else
+    {
+        Console.WriteLine($"Binary Search: Target {target} not found in the array.");
+    }
 }
-else
-{
-    WriteLine($"Binary Search: Target {target} not found in the array.");
-}
+
+// Test with target = 9
+PerformBinarySearch(testArray, 9);
+
+// Test with target = 99
+PerformBinarySearch(testArray, 99);
